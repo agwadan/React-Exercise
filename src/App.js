91099be46ae0bd42411824504226a5718f5a552a
agwa.dan <agwa.dan@gmail.com>
2020-02-16 10:14:15 +0300

    updated partsData.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div>
       <Header/>
-      <Counter />
+      <MainParts/>
       
       <Footer/>
     </div>
--- src/PartsInfo/partsData.js
+++ src/PartsInfo/partsData.js
@@ -26,7 +26,91 @@ const partsData = [
         imgUrl: "img/Brakes3.jpg",
         brand: "Brembo",
         origin: "US"
-    }
+    },
+    {
+        id: 5,
+        name: "Tyres",
+        imgUrl: "",
+        brand: "Road Master",
+        origin: "UK"
+    },
+    {
+        id: 6,
+        name: "Engine Block",
+        imgUrl: "img/EngineBlock1.jpg",
+        brand: "Renault",
+        origin: "Italy"
+    },
+    {
+        id: 7,
+        name: "Engine Block",
+        imgUrl: "img/EngineBlock2.jpg",
+        brand: "Ford",
+        origin: "US"
+    },
+    {
+        id: 8,
+        name: "Rims",
+        imgUrl: "img/Rims2.jpg",
+        brand: "Fogiatos",
+        origin: "France"
+    },
+    {
+        id: 9,
+        name: "Rims",
+        imgUrl: "img/Rims3.png",
+        brand: "Mercedes",
+        origin: "Germany"
+    },
+    {
+        id: 10,
+        name: "Rims",
+        imgUrl: "img/Rims4.jpg",
+        brand: "Roja",
+        origin: "UK"
+    },
+    {
+        id: 11,
+        name: "Supercharger",
+        imgUrl: "img/Supercharger1.jpg",
+        brand: "Hemi",
+        origin: "US"
+    },
+    {
+        id: 12,
+        name: "Supercharger",
+        imgUrl: "img/Supercharger2.jpg",
+        brand: "SRT",
+        origin: "US"
+    },
+    {
+        id: 13,
+        name: "Supercharger",
+        imgUrl: "img/Supercharger3.jpg",
+        brand: "Shelby",
+        origin: "US"
+    },
+    {
+        id: 14,
+        name: "Turbo Charger",
+        imgUrl: "img/Turbo.jpg",
+        brand: "Toyota",
+        origin: "Japan"
+    },
+    {
+        id: 15,
+        name: "Turbo Charger",
+        imgUrl: "img/Turbo2.png",
+        brand: "Nissan",
+        origin: "Japan"
+    },
+    {
+        id: 16,
+        name: "Turbo Charger",
+        imgUrl: "img/Turbo3.jpeg",
+        brand: "Suzuki",
+        origin: "Japan"
+    }   
 ]
 
 export default partsData
