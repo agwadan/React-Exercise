f61b38bb7832bda932c734b79a46b7c257e6b927
agwa.dan <agwa.dan@gmail.com>
2020-04-26 16:41:49 +0300

    Inintialized Covid19 visualizer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
-import Header from './components/Header';
+/* import Header from './components/Header';
 import ToDo from './components/ToDoList/ToDo';
 import Footer from './components/Footer';
 import TimeOfDay from './components/TimeOfDay';
@@ -10,19 +10,31 @@ import MainParts from './PartsInfo/MainParts'
 import MainJokes from './components/Jokes/MainJokes';
 import Counter from './components/Counter';
 import MainCondition from './components/ConditionalRendering/MainCondition';
-import ConditionalRenderingPractice from './components/ConditionalRenderingPractice';
+import ConditionalRenderingPractice from './components/ConditionalRenderingPractice'; */
+
+import {Cards, Chart, CountryPicker} from './components/Covid19';
+import styles from './App.module.css';
+import {fetchData} from './api';
 
 
-function App() {
+
+class App extends React.Component {
+
+  async componentDidCatch(){
+      const data = await fetchData();
+      console.log(data);
+  }
+
+  render(){
   return (
-    <div>
-      <Header/>
-      <ToDo/>
-      
-      <Footer/>
+    <div className={styles.container}>
+      <Cards />
+      <CountryPicker />
+      <Chart />
     </div>
    
   );
+  }
 }
 
 export default App;
