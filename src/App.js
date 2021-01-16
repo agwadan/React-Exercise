77409b7f181ef3393b3af2b5983401009c018433
agwadan <agwa.dan@gmail.com>
2021-01-16 13:29:28 +0300

    Revision: Covered properties.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import logo from './logo.svg';
 //import './App.css';
-/* import Header from './components/Header';
+import Header from './components/Header';
 import ToDo from './components/ToDoList/ToDo';
 import Footer from './components/Footer';
 import TimeOfDay from './components/TimeOfDay';
@@ -9,46 +9,24 @@ import MainParts from './PartsInfo/MainParts'
 import MainJokes from './components/Jokes/MainJokes';
 import Counter from './components/Counter';
 import MainCondition from './components/ConditionalRendering/MainCondition';
-import ConditionalRenderingPractice from './components/ConditionalRenderingPractice'; */
+import ConditionalRenderingPractice from './components/ConditionalRenderingPractice'; 
+import Book from './components/Book';
 
-import {Cards, Chart, CountryPicker} from './components/Covid19';
 import styles from './App.module.css'; //----------------------------------------------------- Styling for the app.
-import {fetchData} from './api';
-import coronaImage from './components/Covid19/Images/image.png';
-
 class App extends React.Component {
-
-  state = {
-    data: {}, //--------------------------------------------------- data object initialized to be filled with data fetched from the api.
-    country: ''
-  }
-
-  async componentDidMount(){
-    const fetchedData = await fetchData();
-    this.setState({data: fetchedData}) //---------------------------- Filling the data object initialized inside state with the data fetched from the api.
-  }
-
-  /* Function to handle change when a different country is picked. */
-  handleCountryChange = async (country) => {
-    const fetchedData = await fetchData(country);
-    this.setState({
-                    data: fetchedData,
-                    country: country}); //------------------------ Assigning fetched data for the specified country to the state.
-  }
 
   render(){
 
-    const {data, country} = this.state;//---------------------------------- Destructuring the data and to be passed to the Cards and chart components.
-
-    return (
-      <div className={styles.container}> 
-        <img className={styles.image} src={coronaImage} alt="CoViD-19"/>
-        <Cards data={data}/>
-        <CountryPicker handleCountryChange={this.handleCountryChange}/>
-        <Chart data={data} country={country}/>
-      </div>
-    );
-  }
+   return(
+     <div>
+       <Header />
+       <Book title='Men are from Mars and Women are from Venus.'/>
+       <Book title='Two and A half Men'/>
+       <Book />
+     </div>
+     
+   );
+};
 }
 
 export default App;
---
+++ src/components/Book.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Book(pips) {
+
+    console.log(pips);
+
+    return (
+        <div>
+            <h1>Agwa</h1>
+            <h2>Daniel</h2>
+        </div>
+    );
+}
+
+export default Book;
