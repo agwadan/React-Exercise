c80b77a73655b03ac00732fc58159ea9f8dcc964
Agwa <agwa.dan@gmail.com>
2021-01-21 11:35:03 +0300

    Revision 03:11:00 - onMouseOver event

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -4,17 +4,21 @@ function Book(pips) {
 
   const { title, origin, imgUrl, children } = pips; //-------------- destructuring the "pips"
 
-  function clickHandler() {
+  function clickHandler(e) {
+    console.log(e);
+    console.log(e.target);
     alert('Awesome....');
   }
 
   //console.log(pips);
   return (
-    <div className='book'>
+    <div className='book' onMouseOver={()=>{
+      console.log(title);
+    }}>
       <h3 onClick={()=>console.log(title)}>{title}</h3>
       <h5>is from {origin}</h5>
       <img src={imgUrl} alt='' />
-      <button type='button' onClick={clickHandler}>
+      <button type='button' onClick={clickHandler}> {/* for functions that are invocked immediately, arrow functions should be used. */}
         Agree
       </button>
     </div >
