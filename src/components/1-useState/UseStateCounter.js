187792c8ba9c3e40344fd0f08664b8f057b1859e
Agwa <agwa.dan@gmail.com>
2021-01-23 13:48:22 +0300

    Revision 04:45:00 - changing state values by passing in functions.

--- src/components/1-useState/UseStateCounter.js
+++ src/components/1-useState/UseStateCounter.js
@@ -4,14 +4,42 @@ const UseStateCounter = () => {
 
   const [value, setValue] = useState(0);
 
+  const complexIncrease = () => {
+    setTimeout(() => {
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+
   return (
-    <section>
-      <h2>Counter</h2>
-      <h1>{value}</h1>
-      <button onClick={() => setValue(value - 1)}>decrease</button>
-      <button onClick={() => setValue(0)}>Reset</button>
-      <button onClick={() => setValue(value + 1)}>increase</button>
-    </section>
+    <div>
+      <section style={{
+        margin: '4rem 0'
+      }
+        , {
+        marginLeft: '4rem'
+      }
+      }>
+        <h2>Counter</h2>
+        <h1>{value}</h1>
+        <button onClick={() => setValue(value - 1)} >decrease</button>
+        <button onClick={() => setValue(0)}>Reset</button>
+        <button onClick={() => setValue(value + 1)}>increase</button>
+      </section>
+
+      <section style={{
+        margin: '4rem 0'
+      }
+        , {
+        marginLeft: '4rem'
+      }
+      }>
+        <h2>Complex Counter</h2>
+        <h1>{value}</h1>
+        <button onClick={()=>complexIncrease()} >Change Value</button>
+      </section>
+    </div>
   );
 }
 
