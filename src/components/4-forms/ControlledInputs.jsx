7b006cf24f84168b1d400debc5ea44b5675f1eed
Agwa <agwa.dan@gmail.com>
2021-01-26 13:14:58 +0300

    Revision 06:30:00 - Covered Basic forms

--- src/components/4-forms/ControlledInputs.jsx
+++ src/components/4-forms/ControlledInputs.jsx
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
 
 export default function ControlledInputs() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('dddd');
+  }
+
   return (
     <div>
       <article>
-        <form className='form'>
-          <div className='form-control'>
-            <label htmlFor='firstName'>Name: </label>
-            <input type='text' id='firstName' name='firstName' />
+        <form className='form' onSubmit={handleSubmit}>
+
+          <div className='form-group'>
+            <label htmlFor='firstName'>Name </label>
+            <input type='text' id='firstName' name='firstName' className='form-control' />
           </div>
+
+          <div className='form-group'>
+            <label htmlFor='firstName'>Email </label>
+            <input type='text' id='email' name='email' className='form-control' />
+          </div>
+
+          <button className=' btn btn-primary' type='submit'>Add Person</button>
         </form>
-      </article>
-    </div>
+      </article >
+    </div >
   )
 }
