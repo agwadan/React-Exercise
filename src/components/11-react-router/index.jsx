b4c708406b577e8de563a68138d8a10f00664bf8
Agwa <agwa.dan@gmail.com>
2021-02-09 00:23:54 +0300

    09:16:06 - Add other routes

--- src/components/11-react-router/index.jsx
+++ src/components/11-react-router/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Setup from '../10-propTypes';
+import About from './about';
 import Home from './home';
 
 const ReactRouterSetup = () => {
   return (
     <Router>
-      <Route path='/'>
+      <Route exact path='/'> {/* Exact ensures this route is accessed if the path matches exaclty what is in the path property. */}
         <Home />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
+      <Route path='/people'>
+        <Setup />
       </Route>
       React Router
     </Router>
