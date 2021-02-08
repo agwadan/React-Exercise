90407cad2d40f38ec4cb03cde87f4c1bc311ded7
Agwa <agwa.dan@gmail.com>
2021-02-08 15:31:46 +0300

    Fetch data in fetchExample

--- src/components/9-customHooks/fetchExample.jsx
+++ src/components/9-customHooks/fetchExample.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+const url = 'https://covid19.mathdro.id/api';
 
 const FetchExample = () => {
+
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const res = await fetch(url);
+    const products = await res.json();
+    setProducts(products);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  console.log(products);
+
   return (
     <div>
-      <h1>Custom Hooks</h1>
+      <h1>{loading ? 'Loading...' : 'Custom Hooks'}</h1>
     </div>
   )
 }
