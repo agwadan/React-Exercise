fe07e188f4730cd1163ccfdd5b7e6012ffd72e81
Agwa <agwa.dan@gmail.com>
2021-02-09 12:10:59 +0300

    Add re-route from error page

--- src/components/11-react-router/error.jsx
+++ src/components/11-react-router/error.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import React from 'react';
 
 const ErrorPage = () => {
   return (
     <div>
       <h1>Error :-(</h1>
+      <Link className='btn btn-primary' to='/'>Return Home</Link>
     </div>
   )
 }
