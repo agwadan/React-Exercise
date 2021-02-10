a99bdbf6e706e2c2c3003b9295a03caa9ac1c470
Agwa <agwa.dan@gmail.com>
2021-02-10 19:39:37 +0300

    Setup count in parent component

--- src/components/12-memoUseMemoUseCallback/index.jsx
+++ src/components/12-memoUseMemoUseCallback/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import { useFetch } from '../9-customHooks/useFetch';
+
+const url = 'https://api.github.com/users'
 
 const MemoIndex = () => {
+
+  const { users } = useFetch(url);
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h2>Memo</h2>
+      <h1>Count: {count}</h1>
+      <button className='btn btn-primary' onClick={() => setCount(count + 1)}>Count Up</button>
     </div>
   )
 }
