ccdf0eac6dc23127171ee77a960d9ee9c84817c3
Agwa <agwa.dan@gmail.com>
2021-02-11 14:03:25 +0300

    Conclude tutorial

--- src/components/9-customHooks/useFetch.js
+++ src/components/9-customHooks/useFetch.js
@@ -1,23 +1,23 @@
 
 //Custom Hooks must begin with the word "use" e.g "useFetch"
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 export const useFetch = (url) => {
 
   const [loading, setLoading] = useState(true);
   const [users, setUsers] = useState([]);
 
-  const getCases = async () => {
+  const getCases = useCallback(async () => {
     const res = await fetch(url);
     const users = await res.json();
     setUsers(users);
     setLoading(false);
-  }
+  }, [url])
 
   useEffect(() => {
     getCases();
-  }, [url]);
+  }, [url, getCases]);
 
   return { loading, users }
 }
