f2effd6c1142355fb586fd5ba0739b70b0f7ac74
Agwa <agwa.dan@gmail.com>
2021-02-17 23:20:35 +0300

    Re-Format navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,29 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class Navbar extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            position: "Header"
-        }
+  constructor() {
+    super()
+    this.state = {
+      position: "Header"
     }
-    render() {
-        return (
-            <nav className='navbar navbar-expand-sm bg-dark navbar-dark sticky-top'>
-                <ul className='navbar-nav'>
-                    <li className='nav-item active'>
-                        <Link className='nav-link' to='/'>Home</Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link className='nav-link' to='/people'>People</Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link className='nav-link' to='/about'>About</Link>
-                    </li>
-                </ul>
-            </nav>
-        )
-    }
+  }
+  render() {
+    return (
+      <nav className='navbar navbar-expand-sm bg-dark navbar-dark sticky-top'>
+        <ul className='navbar-nav'>
+          <li className='nav-item active'>
+            <Link className='nav-link' to='/'>Home</Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/people'>People</Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/about'>About</Link>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
 }
 
-export default Navbar   
+export default Navbar;
