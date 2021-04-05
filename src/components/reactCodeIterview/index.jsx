96094bdb101e6646b0ea5e70995ac1105ba96def
Agwa <agwa.dan@gmail.com>
2021-04-05 12:54:33 +0300

    QN 2: Fetch data from apo

--- src/components/reactCodeIterview/index.jsx
+++ src/components/reactCodeIterview/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { QuestionOne } from './questionOne';
+import { QuestionTwo } from './questionTwo';
 
 export const Interview = () => {
   return (
     <div>
       <h1>Interview</h1>
       <QuestionOne />
+      <QuestionTwo />
     </div>
   )
 }
---
+++ src/components/reactCodeIterview/questionTwo.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const QuestionTwo = () => {
+
+  const [randomDataJSON, setRandomDataJSON] = useState('');
+
+  useEffect(() => {
+    fetchData().then(randomData => {
+      setRandomDataJSON(randomData || 'no data');
+    })
+  }, []);
+
+  const fetchData = () => {
+    return axios.get('https://randomuser.me/api')
+      .then(({ data }) => {
+        console.log(data);
+        return JSON.stringify(data);
+      })
+      .catch(err => console.log(err))
+  };
+
+  return (
+    <div>
+      <h2>Question 2</h2>
+      <h5>Fetch data from the provided api, and display it as a string.</h5>
+      <p>_______________</p>
+      <button onClick={() => fetchData()}>Fetch Random Data</button>
+      <p>{randomDataJSON}</p>
+    </div>
+  )
+}
