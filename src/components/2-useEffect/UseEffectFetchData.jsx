63e90f5f615f9616a6b88b55394d3b41ba9b8e5b
Agwa <agwa.dan@gmail.com>
2021-04-08 13:15:15 +0300

    Add style for fetched api info

--- src/components/2-useEffect/UseEffectFetchData.jsx
+++ src/components/2-useEffect/UseEffectFetchData.jsx
@@ -1,11 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import './users.css';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
+  const [users, setUsers] = useState([]);
+
+  /* FUNCTION TO FETCH THE USERS FROM API ENDPOINT */
+  const GetUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+  }
+
+  /* 
+    USEEFFECT
+    ---------
+    => useEffect cannot return a promise/ can't use async await.
+    */
+  useEffect(() => {
+    GetUsers();
+  }, [])
 
   return (
     <div>
-      <h1>Fetch Data</h1>
+      <h1>GitHub Users</h1>
+      {users.map((user) => {
+        const { id, login, avatar_url, html_url } = user;
+        return (
+          <div>
+            <h4 key={user.id}>{user.login}</h4>
+            <img src={user.avatar_url} />
+          </div>
+        )
+      })}
     </div>
   )
 
